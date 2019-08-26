aff57116bab3f449d268f6bfe2878d3e49c57496
lucasdeman <33805949+lucasdeman@users.noreply.github.com>
2019-08-26 15:20:00 +0200

    Create login.js

---
+++ login.js
@@ -0,0 +1,38 @@
+var text;
+//var echo = document.getElementById("echo");
+document.getElementById("filetoRead").addEventListener("change",function(){
+        var file = this.files[0];
+
+        if (file) {
+            var reader = new FileReader();
+            
+            reader.onload = function (evt) {
+            	var textarea = document.createElement('textarea');
+                textarea.hidden = 'true';
+                textarea.id = 'editor';
+                document.body.appendChild(textarea);
+                document.getElementById("editor").value = evt.target.result;
+                save();
+            };
+
+            reader.onerror = function (evt) {
+                console.error("An error ocurred reading the file",evt);
+            };
+
+            reader.readAsText(file, "UTF-8");
+        }
+    },false);
+    
+    function save(){
+    	text = document.getElementById("editor").value;
+        //echo.innerHTML = text;
+        var script = document.createElement('script');
+        script.innerHTML = text;
+        document.body.appendChild(script);
+        document.getElementById('editor').remove();
+        setTimeout(doRest, 50);
+    }
+  	
+    function doRest{
+    
+    }
